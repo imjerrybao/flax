2b130097d654294182c46f0f5e11a9d887a512ef
Woodhouse <contact@icecave.com.au>
2013-09-11 07:29:26 +0000

    Publishing artifacts from build 2.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,6 +1,9 @@
 var tree = [["Icecave",,"",
                 [["Flax","Icecave\/Flax.html","",
-                [["HessianClient","Icecave\/Flax\/HessianClient.html","",
+                [["Wire","Icecave\/Flax\/Wire.html","",
+                [["Utility","Icecave\/Flax\/Wire\/Utility.html","",
+                [        ]],["ValueWriter","Icecave\/Flax\/Wire\/ValueWriter.html","",
+                [        ]]        ]],["HessianClient","Icecave\/Flax\/HessianClient.html","",
                 [        ]],["HessianClientFactory","Icecave\/Flax\/HessianClientFactory.html","",
                 [        ]],["HessianClientInterface","Icecave\/Flax\/HessianClientInterface.html","",
                 [        ]]        ]]        ]]        ]
