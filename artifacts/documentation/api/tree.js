99977e6672bc6c87b632e0984698212545b3707b
Woodhouse <contact@icecave.com.au>
2013-09-26 22:55:39 +0000

    Publishing artifacts from build 39.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -24,7 +24,6 @@ var tree = [["Icecave",,"",
                 [        ]],["HessianClient","Icecave\/Flax\/HessianClient.html","",
                 [        ]],["HessianClientFactory","Icecave\/Flax\/HessianClientFactory.html","",
                 [        ]],["HessianClientInterface","Icecave\/Flax\/HessianClientInterface.html","",
-                [        ]],["Object","Icecave\/Flax\/Object.html","",
                 [        ]],["PackageInfo","Icecave\/Flax\/PackageInfo.html","",
                 [        ]]        ]]        ]]        ]
 
